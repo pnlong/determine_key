b3e6998c00ae96e178215c92303481be522423f0
Phillip Long <phillipnlong@gmail.com>
2023-09-05 00:23:21 -0700

    added determine_key

---
+++ determine_key.py
@@ -0,0 +1,16 @@
+# README
+# Phillip Long
+# September 5, 2023
+
+# Given a song, output its predicted key.
+
+# python ./determine_key.py key_class_nn_filepath key_quality_nn_filepath song_filepath
+
+
+# IMPORTS
+##################################################
+import sys
+# sys.argv = ("./determine_key.py", "/Users/philliplong/Desktop/Coding/artificial_dj/data/key_class_nn.pth", "/Users/philliplong/Desktop/Coding/artificial_dj/data/key_quality_nn.pth", "")
+##################################################
+
+
